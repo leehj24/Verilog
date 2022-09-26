601029055c630543e53da63b8bbba2a32d68d5ea
leehj24 <hyunji4579@gmail.com>
2022-09-26 13:47:52 +0900

    1주차

---
+++ 1주차/prj.v
@@ -0,0 +1,15 @@
+module	halfadder_tb;
+  wire	sum, cout;
+  reg	a, b;
+  halfadder u1 (sum, cout, a, b);
+  initial begin
+    $monitor("%d: %b	%b : %b%b", $time, a, b, cout, sum);
+    #100 $finish;
+  end
+  initial begin
+    #20 a=0; b=0;	
+    #20 b=1;	 
+    #20 a=1;	 
+    #20 b=0;	
+  end
+endmodule
